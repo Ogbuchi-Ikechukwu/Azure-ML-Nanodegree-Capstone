15bd158b2a896a9af8fee4de3b734b16edcdf524
Ikechukwu Nigel Ogbuchi <53430747+Ogbuchi-Ikechukwu@users.noreply.github.com>
2021-01-28 02:14:32 +0000

    Add files via upload

--- starter_file/train.py
+++ starter_file/train.py
@@ -1,58 +1,60 @@
-from sklearn.linear_model import LogisticRegression
 import argparse
-import os
-import numpy as np
-from sklearn.metrics import mean_squared_error
 import joblib
-from sklearn.model_selection import train_test_split
+import numpy as np
+import os
 import pandas as pd
-from azureml.core.run import Run#1
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+
+from azureml.core import Dataset, Datastore
+from azureml.core import Workspace, Experiment
+from azureml.core.run import Run
+from azureml.data.datapath import DataPath
 from azureml.data.dataset_factory import TabularDatasetFactory
-from azureml.core import Dataset
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-
-run = Run.get_context()
-
-def clean_data(data):
-   
-    x_df = data.to_pandas_dataframe()
-    
-    y_df = x_df.pop("Claim")
-
-    return x_df,y_df
-
-# data = pd.read_csv("heart_failure_clinical_records_dataset.csv")
-data = "https://raw.githubusercontent.com/Ogbuchi-Ikechukwu/Azure-ML-Nanodegree-Capstone/master/starter_file/train_data.csv"
-
-ds = Dataset.Tabular.from_delimited_files(data)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=403,shuffle=True) # 1 
-
-run = Run.get_context()
 
 def main():
-    # Add arguments to script
+    # Get run context
+    run = Run.get_context()
+    
+    # Parse data
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-f')
-    
+    parser.add_argument('--data_url', type=str, default='https://raw.githubusercontent.com/Ogbuchi-Ikechukwu/Azure-ML-Nanodegree-Capstone/master/starter_file/train_data_cleaned.csv', help='URL of the dataset to be used')
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
-    primary_metric_name="Accuracy"
     args = parser.parse_args()
 
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
+    run.log("Regularization Strength", np.float(args.C))
+    run.log("Max iterations", np.int(args.max_iter))
+
+    # Get the dataset
+    dataset = Dataset.Tabular.from_delimited_files(args.data_url)
+
+    # Drop non-numeric columns from the dataset
+    df = dataset.to_pandas_dataframe()
+    df = df.drop(['Customer Id', 'Building_Painted', 'Building_Fenced','Garden','Settlement'], axis=1)
+
+    # Separate x and y columns. 'Survived' is y, because it is was we want to predict
+    y_df = df['Claim']
+    df.pop('Claim')
+    x_df = df
+
+    # Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size=0.25)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-    joblib.dump(model,'outputs/model.joblib')
+
     accuracy = model.score(x_test, y_test)
-    
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('outputs', exist_ok=True)
+    # files saved in the "outputs" folder are automatically uploaded into run history
+    joblib.dump(model, 'outputs/model.joblib')
+
 if __name__ == '__main__':
     main()
